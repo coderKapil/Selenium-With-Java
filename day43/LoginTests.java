3514801a6fde47449ab8e275542c720ed0da8747
Kapil Rana <kapsrana56@gmail.com>
2025-11-09 19:48:15 +0530

    TestNG dependency methods and grouping

---
+++ day43/LoginTests.java
@@ -0,0 +1,19 @@
+package day43;
+
+import org.testng.annotations.Test;
+
+public class LoginTests {
+
+	@Test(priority=1,groups = {"sanity"})
+	void loginByEmail() {
+		System.out.println("this is login by email.....");
+	}
+	@Test(priority=2,groups = {"sanity"})
+	void loginByFacebook() {
+		System.out.println("this is login by facebook.....");
+	}
+	@Test(priority=3,groups = {"sanity"})
+	void loginByTwitter() {
+		System.out.println("this is login by twitter.....");
+	}
+}
---
+++ day43/SignUpTests.java
@@ -0,0 +1,19 @@
+package day43;
+
+import org.testng.annotations.Test;
+
+public class SignUpTests {
+
+	@Test(priority=1,groups = {"regression"})
+	void signupbyemail() {
+		System.out.println("this is signup by email.....");
+	}
+	@Test(priority=2,groups = {"regression"})
+	void signupbyfacebook() {
+		System.out.println("this is signup by facebook.....");
+	}
+	@Test(priority=3,groups = {"regression"})
+	void signupbytwitter() {
+		System.out.println("this is signup by twitter.....");
+	}
+}
